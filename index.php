c2a37a1372d71aa7323b3a8d74286b2f86fa2128
paulooolll <82038453+paulooolll@users.noreply.github.com>
2022-04-14 17:40:59 +0800

    2022/4/14 Paul creat a new project

---
+++ index.php
@@ -0,0 +1,10 @@
+<form action="user.php" method="post">
+	使用者名稱:
+	<input type="text" name="my_name"  /> 
+	<br />
+	密碼:
+	<input type="text" name="my_password"  /> 
+	<br />
+	登入:
+	<input type="submit" name="submit" value="login" />
+</form>
---
+++ user.php
@@ -0,0 +1,10 @@
+<form action="index.php" method="post">
+中文:
+<input type="text" name=""  /> 
+<br />
+英文:
+<input type="text" name=""  /> 
+<br />
+確認:
+<input type="submit" name="submit" value="login" />
+</form>
